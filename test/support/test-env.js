2207d497eb855e5fb13dc6f4889ff4f739c706ae
Marcin Mazurek <marcin.mazurek@which.co.uk>
2016-01-24 10:45:52 +0000

    Add JSX assertions library

--- test/support/test-env.js
+++ test/support/test-env.js
@@ -1,2 +1,6 @@
-require('chai').should();
+let chai = require('chai');
+let jsxChai = require('chai-equal-jsx');
 global.using = require('jasmine-data-provider');
+
+chai.should();
+chai.use(jsxChai);
