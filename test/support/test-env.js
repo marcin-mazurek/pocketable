9cf4a80fb5e8489b2a01108f2f35eb53d953af84
Marcin Mazurek <marcin.mazurek@which.co.uk>
2016-01-25 00:31:10 +0000

    Use isomorphic-fetch as a polyfill for React Native environment

--- test/support/test-env.js
+++ test/support/test-env.js
@@ -1,6 +1,7 @@
 let chai = require('chai');
 let jsxChai = require('chai-equal-jsx');
 global.using = require('jasmine-data-provider');
+global.fetch = require('isomorphic-fetch');
 
 chai.should();
 chai.use(jsxChai);
