c5c070539658c4096b2fd773c2cb66738f758796
Marcin Mazurek <marcin.mazurek@which.co.uk>
2016-01-30 00:46:11 +0000

    Remove test helpers (incompatible with React Native)

--- test/support/test-helpers.js
+++
@@ -1,9 +0,0 @@
-import React from './mocked-react';
-import TestUtils from 'react-addons-test-utils';
-
-const renderer = TestUtils.createRenderer();
-
-export function renderShallow(component) {
-  renderer.render(component);
-  return renderer.getRenderOutput();
-}
