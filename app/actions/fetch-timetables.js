5fb1c1105a00f5f5397ecbfee8395b998d5bf27f
Marcin Mazurek <marcin.mazurek@which.co.uk>
2016-01-30 13:58:59 +0000

    Update fetchTimetables action

--- app/actions/fetch-timetables.js
+++ app/actions/fetch-timetables.js
@@ -3,5 +3,5 @@ import updateStop from './update-stop';
 export default function() {
   updateStop('940GZZLUNHT', 'Eastbound - Platform 2');
   updateStop('940GZZLURGP', 'Southbound - Platform 2');
-  updateStop('940GZZLUOXC', 'Eastbound - Platform 2');
+  updateStop('940GZZLUOXC', 'Westbound - Platform 1');
 }
