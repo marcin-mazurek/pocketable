1465984f8168a8a87c5294946aa3f96cb856ec7e
Marcin Mazurek <marcin.mazurek@which.co.uk>
2016-03-08 12:56:52 +0000

    Add Enzyme - testing utility for React components and required dependencies, update and comment test environment setup file

--- test/support/test-env.js
+++ test/support/test-env.js
@@ -1,7 +1,16 @@
-let chai = require('chai');
-let jsxChai = require('chai-equal-jsx');
-global.using = require('jasmine-data-provider');
-global.fetch = require('isomorphic-fetch');
+// Mutates `react-native` package require cache
+// Allows for native components unit testing in Node.js environment
+require('react-native-mock/mock');
 
+// Exposes React - required implicitly by Enzyme
+global.React = require('react');
+
+// Makes Chai's should-style assertions available globally
+var chai = require('chai');
 chai.should();
-chai.use(jsxChai);
+
+// Adds utility for parametrized tests globally
+global.using = require('jasmine-data-provider');
+
+// Node.js polyfill for fetch API available in React Native
+global.fetch = require('isomorphic-fetch');
