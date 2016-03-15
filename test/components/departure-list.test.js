7f9f05af6017ff5bd517911008de5de36390f9ac
Marcin Mazurek <marcin.mazurek@which.co.uk>
2016-03-15 16:36:39 +0000

    Test DepartureList component

---
+++ test/components/departure-list.test.js
@@ -0,0 +1,36 @@
+import {shallow} from 'enzyme';
+import DepartureList from '../../app/components/departure-list/departure-list';
+import InitialLoading from '../../app/components/initial-loading/initial-loading';
+import NoDepartures from '../../app/components/no-departures/no-departures';
+import Departure from '../../app/components/departure/departure';
+
+describe('<DepartureList />', () => {
+  it('renders <InitialLoading /> when departures are not loaded yet (are undefined)', () => {
+    const component = shallow(<DepartureList departures={undefined} />);
+    component.contains(<InitialLoading />).should.be.true;
+  });
+
+  it('renders <NoDepartures /> when there is no departures', () => {
+    const component = shallow(<DepartureList departures={[]} />);
+    component.contains(<NoDepartures />).should.be.true;
+  });
+
+  it('renders <Departure /> component for each departure', () => {
+    const component = shallow(
+      <DepartureList departures={[
+        { towards: 'Epping', currentLocation: 'South Ruislip', minutesToArrival: 3 },
+        { towards: 'Woodford', currentLocation: 'Rusilip Gardens', minutesToArrival: 5 }
+      ]} />
+    );
+
+    const departureList = component.find('View');
+
+    departureList.children().length.should.equal(2);
+    departureList.contains(
+      <Departure towards="Epping" currentLocation="South Ruislip" minutesToArrival={3} />
+    ).should.be.true;
+    departureList.contains(
+      <Departure towards="Woodford" currentLocation="Rusilip Gardens" minutesToArrival={5} />
+    ).should.be.true;
+  });
+});
