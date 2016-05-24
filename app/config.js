bfdd344f665a634e83ed71c3c3b48fc9d76f23d8
Marcin Mazurek <marcin.mazurek@which.co.uk>
2016-05-24 13:05:19 +0100

    Update application configuration

--- app/config.js
+++ app/config.js
@@ -1,7 +1,11 @@
 export default {
+  // colours: http://oobrien.com/2012/01/tube-colours/
   stops: {
-    '940GZZLUOXC': { name: 'Oxford Circus', line: 'Central line', background: '#E32017' },
-    '940GZZLUNHT': { name: 'Northolt', line: 'Central line', background: '#E32017' },
-    '940GZZLURGP': { name: `Regent's Park`, line: 'Bakerloo line', background: '#B36305' }
+    '940GZZLUHOH': { name: 'Harrow on the Hill', direction: 'Southbound', lineId: 'metropolitan', line: 'Metropolitan line', background: '#9B0056' },
+    '940GZZLUGPS': { name: 'G. Portland Street', direction: 'Westbound', lineId: 'metropolitan', line: 'Metropolitan line', background: '#9B0056' },
+    '940GZZLUOXC': { name: 'Oxford Circus', direction: 'Westbound', lineId: 'central', line: 'Central line', background: '#E32017' },
+    '940GZZLUNHT': { name: 'Northolt', direction: 'Eastbound', lineId: 'central', line: 'Central line', background: '#E32017' },
+    '940GZZLURGP': { name: `Regent's Park`, direction: 'Southbound', lineId: 'bakerloo', line: 'Bakerloo line', background: '#B36305' }
   }
 };
+  
